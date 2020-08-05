5e99337753b0d458e09bfc5098f0c667ce56322a
Damian Federico <damianfederico@protonmail.com>
2020-08-05 20:25:12 -0300

    Initialize Godot project + Add movement script to player

---
+++ Scenes/Player/Character.gd
@@ -0,0 +1,49 @@
+extends KinematicBody
+
+export var vel = 10
+var acel = 5
+
+var mouse_sens = 0.5 
+
+var direccion = Vector3()
+var velocity = Vector3()
+
+onready var pivot = $Pivot
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		# deg2rad change float degres to radones, related to 3D physics
+		rotate_y(deg2rad(-event.relative.x * mouse_sens))
+		# Move camera(pivot) vertically. rotate is what you are goint to move
+		pivot.rotate_x(deg2rad(-event.relative.y * mouse_sens))
+		# Limit the max and min angle degrees camera. rotation is what was already rotated
+		pivot.rotation.x = clamp(pivot.rotation.x, deg2rad(-90), deg2rad(90))
+
+func _process(delta):
+	
+	#Every frame we are assigning a Vector3 to direccion. If it's not here it going to throw errors
+	direccion = Vector3() 
+	
+	if Input.is_action_pressed("move_forward"):
+		# Basis is related to matrix tranform 
+		# in z we rest to going forward
+		direccion -= transform.basis.z
+	elif Input.is_action_pressed("move_backward"):
+		direccion += transform.basis.z
+	
+	if Input.is_action_pressed("move_left"):
+		direccion -= transform.basis.x
+	elif Input.is_action_pressed("move_right"):
+		direccion += transform.basis.x
+	
+	# We normalize the direccion
+	direccion = direccion.normalized()
+	velocity = direccion * vel
+	# Second argument is time. If we omit interpolate, the movement less subtle (brusco)
+	velocity.linear_interpolate(velocity, acel * delta)
+	move_and_slide(velocity, Vector3.UP)
