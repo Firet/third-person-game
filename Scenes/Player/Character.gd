9fa6a0b747524e9d27751d940c75016fe9aa1739
Damian Federico <damianfederico@protonmail.com>
2020-08-10 13:56:31 -0300

    Refactor Character.gd, add move_camera_direction and move_player methods

--- Scenes/Player/Character.gd
+++ Scenes/Player/Character.gd
@@ -17,7 +17,31 @@ func _ready():
 
 func _input(event):
 	set_mouse_visible()
-		
+	move_camera_direction(event)
+
+func _process(delta):
+	
+	#Every frame we are assigning a Vector3 to direccion. If it's not here it going to throw errors
+	direccion = Vector3() 
+	move_player()
+	# We normalize the direccion
+	direccion = direccion.normalized()
+	velocity = direccion * vel
+	# Second argument is time. If we omit interpolate, the movement less subtle (brusco)
+	velocity.linear_interpolate(velocity, acel * delta)
+	move_and_slide(velocity, Vector3.UP)
+
+
+func set_mouse_captured():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func set_mouse_visible():
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func move_camera_direction(event):
 	if event is InputEventMouseMotion:
 		# deg2rad change float degres to radones, related to 3D physics
 		rotate_y(deg2rad(-event.relative.x * mouse_sens))
@@ -26,11 +50,7 @@ func _input(event):
 		# Limit the max and min angle degrees camera. rotation is what was already rotated
 		pivot.rotation.x = clamp(pivot.rotation.x, deg2rad(-90), deg2rad(90))
 
-func _process(delta):
-	
-	#Every frame we are assigning a Vector3 to direccion. If it's not here it going to throw errors
-	direccion = Vector3() 
-	
+func move_player():
 	if Input.is_action_pressed("move_forward"):
 		# Basis is related to matrix tranform 
 		# in z we rest to going forward
@@ -42,17 +62,3 @@ func _process(delta):
 		direccion -= transform.basis.x
 	elif Input.is_action_pressed("move_right"):
 		direccion += transform.basis.x
-	
-	# We normalize the direccion
-	direccion = direccion.normalized()
-	velocity = direccion * vel
-	# Second argument is time. If we omit interpolate, the movement less subtle (brusco)
-	velocity.linear_interpolate(velocity, acel * delta)
-	move_and_slide(velocity, Vector3.UP)
-
-func set_mouse_captured():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func set_mouse_visible():
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
