64261c72c019b1db65eaf59b462a094501427e37
Damian Federico <damianfederico@protonmail.com>
2020-08-13 20:33:36 -0300

    Rename character.gd, refactor move_player method and rename all variables according to godot documentation

--- Scenes/Player/Character.gd
+++ Scenes/Player/Character.gd
@@ -1,16 +1,15 @@
 extends KinematicBody
 
-export var gravityForce = 20
-export var jumpForce = 10
-export var vel = 10
-var acel = 5
-var numberOfJumps = 0
+export var gravity_force = 20
+export var jump_force = 10
+export var movement_velolocity = 10
 
-var mouse_sens = 0.5 
+var movement_aceleration = 5
+var number_of_jumps = 0
+var mouse_sensitivity = 0.5 
 
-var direccion = Vector3()
-var velocity = Vector3()
-var verticalDirection = Vector3()
+var movement_vector = Vector3()
+var vertical_vector = Vector3()
 
 onready var pivot = $Pivot
 
@@ -23,18 +22,10 @@ func _input(event):
 	set_mouse_visible()
 	move_camera_direction(event)
 
-func _process(delta):
-	#Every frame we are assigning a Vector3 to direction. If it's not here it going to throw errors
-	direccion = Vector3() 
-	move_player()
+
+func _process(delta): 
+	move_player(delta)
 	jump(delta)
-	
-	# We normalize the direccion
-	direccion = direccion.normalized()
-	velocity = direccion * vel
-	# Second argument is time. If we omit interpolate, the movement less subtle (brusco)
-	velocity.linear_interpolate(velocity, acel * delta)
-	move_and_slide(velocity, Vector3.UP)
 
 
 func set_mouse_captured():
@@ -49,40 +40,51 @@ func set_mouse_visible():
 func move_camera_direction(event):
 	if event is InputEventMouseMotion:
 		# deg2rad change float degres to radones, related to 3D physics
-		rotate_y(deg2rad(-event.relative.x * mouse_sens))
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 		# Move camera(pivot) vertically. rotate is what you are goint to move
-		pivot.rotate_x(deg2rad(-event.relative.y * mouse_sens))
+		pivot.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 		# Limit the max and min angle degrees camera. rotation is what was already rotated
 		pivot.rotation.x = clamp(pivot.rotation.x, deg2rad(-90), deg2rad(90))
 
-func move_player():
+
+func move_player(delta):
+	#Every frame we are assigning a Vector3 to direction. If it's not here it going to throw errors
+	var movementDirection = Vector3()
 	if Input.is_action_pressed("move_forward"):
 		# Basis is related to matrix tranform 
 		# in z we substract to going forward
-		direccion -= transform.basis.z
+		movementDirection -= transform.basis.z
 	elif Input.is_action_pressed("move_backward"):
-		direccion += transform.basis.z
+		movementDirection += transform.basis.z
 	
 	if Input.is_action_pressed("move_left"):
-		direccion -= transform.basis.x
+		movementDirection -= transform.basis.x
 	elif Input.is_action_pressed("move_right"):
-		direccion += transform.basis.x
+		movementDirection += transform.basis.x
+
+	# We normalize the movementDirection
+	movementDirection = movementDirection.normalized()
+	movement_vector = movementDirection * movement_velolocity
+	# Second argument is time. If we omit interpolate, the movement less subtle
+	movement_vector.linear_interpolate(movement_vector, movement_aceleration * delta)
+	move_and_slide(movement_vector, Vector3.UP)
+
 
 func jump(delta):
-	move_and_slide(verticalDirection, Vector3.UP)
+	move_and_slide(vertical_vector, Vector3.UP)
 	
 	if is_on_floor():
-		numberOfJumps = 0
+		number_of_jumps = 0
 
 	if not is_on_floor():
-		verticalDirection.y -= gravityForce * delta
+		vertical_vector.y -= gravity_force * delta
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		if numberOfJumps == 0:
-			verticalDirection.y  = jumpForce
-			numberOfJumps = 1
+		if number_of_jumps == 0:
+			vertical_vector.y  = jump_force
+			number_of_jumps = 1
 	
 	if Input.is_action_just_pressed("jump") and not is_on_floor():
-		if numberOfJumps == 1:
-			verticalDirection.y = jumpForce
-			numberOfJumps = 2
+		if number_of_jumps == 1:
+			vertical_vector.y = jump_force
+			number_of_jumps = 2
